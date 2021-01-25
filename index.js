a92282db3e2c95f06d8a303a6e42334e9dae6552
Nipsu96 <n.lahteinen1@gmail.com>
2021-01-25 14:59:12 +0200

    path things2

--- index.js
+++ index.js
@@ -384,6 +384,6 @@ app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3005,() => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
